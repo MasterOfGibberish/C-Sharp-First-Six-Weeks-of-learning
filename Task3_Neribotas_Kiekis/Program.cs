3cc8c47dc68aafd867fb8eadf46ef19246b21678
MasterOfGibberish <92628961+MasterOfGibberish@users.noreply.github.com>
2021-10-17 18:00:47 +0300

    Add files via upload

---
+++ Task3_Neribotas_Kiekis/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3_Neribotas_Kiekis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> SkaiciuSarasas = new List<int>();
+            Console.WriteLine("Iveskite skaicius");
+            int skaicius = Convert.ToInt32(Console.ReadLine());
+            if (skaicius == 0)
+            {
+                SkaiciuSarasas.Remove(0);
+            }
+            else
+            {
+                SkaiciuSarasas.Add(skaicius);
+            }
+            while (skaicius != 0)
+            {
+                if (skaicius == 0)
+                {
+                    SkaiciuSarasas.Remove(0);
+                }
+                else 
+                {
+                    Console.WriteLine("Iveskite skaicius");
+                    skaicius = Convert.ToInt32(Console.ReadLine());
+                    SkaiciuSarasas.Add(skaicius);
+                    SkaiciuSarasas.Remove(0);
+                }
+            }
+            SkaiciuSarasas.Sort();
+            foreach (var Skaiciai in SkaiciuSarasas)
+            {
+                Console.Write(Skaiciai);
+            }
+            Console.WriteLine("Metas Pailseti");
+            Console.ReadKey();
+        }
+    }
+}
